2dd978dd22696bdbf2b8c1a0ae2e7f6d9d5eb983
Chanthy Ngin <nginchanthy@gmail.com>
2018-02-27 16:29:34 -0800

    show detail first version

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-    for(var i = 1; i < 200; i++) {
-        $("body").append(
-            "<img src='http://pokeapi.co/media/sprites/pokemon/" + i +  ".png' alt='" + i + "'>"
+    for(var i = 1; i <= 151; i++) {
+        $("#pokemons").append(
+            "<img id='" + i + "' src='http://pokeapi.co/media/sprites/pokemon/" + i +  ".png' alt='" + i + "'>"
         );
     }
+
+    $("img").click(function(){
+        
+        //http://pokeapi.salestock.net/api/v2/
+        //https://cors.now.sh/https://pokeapi.co/api/v2/ --> NOt working
+        //http://pokeapi.co/api/v2 -->has been blocked by CORS policy
+        var apiUrl = "http://pokeapi.salestock.net/api/v2/pokemon/" + $(this).attr("id")
+        $.get(apiUrl, function(res) {
+            //console.log(res);
+            $("#detail").html(
+                "<h1>" + res.name + "</h1>"
+                + "<img src='" + res.sprites.front_default + "'" + " >"
+                + "<h3>Types: " + res.types[0].type.name + "</he>"
+                + "<h3>Height: " + res.height + "</h3>"
+                + "<h3>Weight: " + res.weight + "</he>"
+            );
+        }, "json");
+        
+    });
 });
